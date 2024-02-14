6a43d1d459d11cabb099db22b67e662ce53c99a6
ynishimi <nishimi.yuki.37z@st.kyoto-u.ac.jp>
2024-02-14 21:13:20 +0900

    Initial Commit

---
+++ 575/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  575
+//
+//  Created by Yuki Nishimi on 2024/02/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 575/_75App.swift
@@ -0,0 +1,17 @@
+//
+//  _75App.swift
+//  575
+//
+//  Created by Yuki Nishimi on 2024/02/14.
+//
+
+import SwiftUI
+
+@main
+struct _75App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
